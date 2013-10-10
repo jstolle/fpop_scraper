f38d9bb535a6d524532fc58ba63776a9d85485c9
William Washco <xx3nvyxx@gmail.com>
2013-10-10 16:08:43 -0700

    Actually echo the variables.

--- freedompop_usage.php
+++ freedompop_usage.php
@@ -11,29 +11,20 @@
   
   <head>
     <title>
-      <?php $user ?>'s Freedompop Usage Stats
+      <?=$user ?>'s Freedompop Usage Stats
     </title>
   </head>
 
   <body>
-    <h2>Freedompop usage for <?php $user ?></h2>
-    <p>
-      Days Left:
-      <?php
-        $details->days_left;
-      ?>
-    </p>
-    <p>
-      Data Used:
-      <?php
-        $details->data_used;
-      ?>
-    </p>
-    <p>
-      Total Data Available:
-      <?php
-        $details->total_data;
-      ?>
-    </p>
+      <h2>Freedompop usage for <?=$user ?></h2>
+      <p>
+        Days Left: <?=$details->days_left ?>
+      </p>
+      <p>
+        Data Used: <?=$details->data_usage ?>
+      </p>
+      <p>
+        Total Data Available: <?=$details->total_data ?>
+      </p>
   </body>
 </html>
